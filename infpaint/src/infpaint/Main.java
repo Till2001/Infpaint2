35ad1fc38f267c417f595df45320c5722efe834c
Till2001 <tillwege@gmail.com>
2018-01-13 13:04:20 +0100

    Start

---
+++ infpaint/src/infpaint/Main.java
@@ -0,0 +1,11 @@
+package infpaint;
+public class Main {
+
+	public static void main(String[] args) {
+		Paint paint;
+		paint = new Paint();
+		paint.main();
+
+	}
+
+}
---
+++ infpaint/src/infpaint/Paint.java
@@ -0,0 +1,23 @@
+package infpaint;
+import basiX.*;
+
+
+public class Paint {
+	
+	private Fenster f;
+	
+	
+
+	public Paint() {
+		f = new Fenster();
+	}		
+
+
+	
+	public void main() {
+		
+	}
+	
+	
+}
+	
